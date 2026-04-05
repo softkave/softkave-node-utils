088f22d108bcfc71b79374fd867fc4d9a1a37eba
Abayomi Akintomide <ywordk@gmail.com>
2026-04-05 13:05:42 +0100

    Removed fimidx in favor of users interacting with fimidx directly

--- src/common/logger/fimidx-auth-logger.ts
+++
@@ -1,7 +0,0 @@
-import {FimidxNextAuthLogger} from 'fimidx';
-import {fimidxConsoleLogger} from './fimidx-console-logger.js';
-
-export const fimidxNextAuthLogger = new FimidxNextAuthLogger({
-  fimidxConsoleLogger,
-  debug: false,
-});
